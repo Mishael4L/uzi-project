06d339ea2cde9e805fb98bec2ad2cda0149b1446
mishael4l <olaoyeoluwakayode@gmail.com>
2026-03-04 18:06:43 +0100

    Separate static and media file storage

--- Uzipro/settings.py
+++ Uzipro/settings.py
@@ -154,8 +154,9 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
 if ENVIRONMENT == 'production':
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
     DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 else:
     MEDIA_URL = '/media/'
