dd1120006ab80b8da3948f434dddf6a244bdc0e2
mishael4l <olaoyeoluwakayode@gmail.com>
2026-03-05 18:04:52 +0100

    Add WHITENOISE_MANIFEST_STRICT=False

--- Uzipro/settings.py
+++ Uzipro/settings.py
@@ -151,6 +151,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static',]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+WHITENOISE_MANIFEST_STRICT = False
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
