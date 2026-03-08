f4e5e603562af9b714c48c89812669ff976326c8
mishael4l <olaoyeoluwakayode@gmail.com>
2026-03-08 17:30:52 +0100

    Use CompressedStaticFilesStorage to avoid strict checks

--- Uzipro/settings.py
+++ Uzipro/settings.py
@@ -158,7 +158,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # WHITENOISE_KEEP_ONLY_HASHED_FILES = False
 
 if ENVIRONMENT == 'production':
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
     WHITENOISE_MANIFEST_STRICT = False
 else:
     STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
